07a7cb623bc1761604fd115d6e4cebdc4308ec0e
Renato Amaral <srkimbo@gmail.com>
2018-02-15 21:26:41 +0000

    My version of Map was created

--- mapMyv.js
+++ mapMyv.js
@@ -1,13 +1,36 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-map(words, function(word) {
+var a = map(words, function(word) {
   return word.length;
 });
 
-map(words, function(word) {
+var b = map(words, function(word) {
   return word.toUpperCase();
 });
 
-map(words, function(word) {
+var c = map(words, function(word) {
   return word.split('').reverse().join('');
 });
+
+
+
+function map(words, callback){
+  var results =[]
+   for (var i = 0; i < words.length; i++) {
+     results.push(callback(words[i]))
+   }
+   return results;
+
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// [6, 7, 2, 5, 3]
+
+// [ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
+
+// [ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
+
+
